a6069b3d07f257681f051778ca0cd25b44e8bdfd
MayronSouza <mayron.souza@gmz.solutions>
2023-05-18 18:35:37 -0300

    initial commit

---
+++ src/test/java/br/com/mayronsouza/api/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mayronsouza.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
